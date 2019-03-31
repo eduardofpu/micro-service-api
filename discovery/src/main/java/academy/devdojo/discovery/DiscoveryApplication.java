7dd8a15c784c3b40ab3973669575c75c7f08ea3d
eduardo <eduardo27_minotauro>
2019-03-31 11:50:59 -0300

    resolvendo dependencias

--- discovery/src/main/java/academy/devdojo/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/academy/devdojo/discovery/DiscoveryApplication.java
@@ -2,11 +2,12 @@ package academy.devdojo.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 //spring-boot:run -Dspring.profiles.active=zone1 -Dserver.port=8081
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class DiscoveryApplication {
 
